7d566b3f1c765b2461dd828f16f7da16db5167ee
ankur-vunet <107390060+ankur-vunet@users.noreply.github.com>
2024-08-01 15:03:13 +0530

    config js update basic

--- vunet-docs/docusaurus.config.js
+++ vunet-docs/docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'VuNet-Docs',
+  tagline: 'Welcome to VuNet Documentation!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ankur-vunet.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ankur-vunet', // Usually your GitHub org/user name.
+  projectName: 'vunet-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -43,9 +43,12 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          /*
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          */
         },
+        
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
@@ -66,7 +69,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'VuNet-Docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
